2e8c26c918f19187afb8c78366dd1ad3a23ed1a0
Aashish Kumar <aashish.kumar1@byjus.com>
2021-07-11 12:34:14 +0530

    refactor

--- src/make_real_image.py
+++ src/make_real_image.py
@@ -3,9 +3,6 @@ import cv2
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
 import glob
 from random import randrange
 from tqdm import tqdm
