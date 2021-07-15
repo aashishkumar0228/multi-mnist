d1e43054cb1ee2d56e6ef575caf96d8030e53b6f
Aashish Kumar <aashish.kumar1@byjus.com>
2021-07-15 20:06:07 +0530

    new data path

--- src/config.py
+++ src/config.py
@@ -2,10 +2,10 @@ output_dir = "../saved_models/"
 figure_folder = "../figures/"
 
 # dataset paths
-train_df_path =         "/home/kaushikdas/aashish/multi-digit-dataset/output_3/train/labels_1_to_8.csv"
-train_image_base_path = "/home/kaushikdas/aashish/multi-digit-dataset/output_3/train/combined_1_to_8_random_real/"
-test_df_path =          "/home/kaushikdas/aashish/multi-digit-dataset/output_3/test/labels_1_to_8.csv"
-test_image_base_path =  "/home/kaushikdas/aashish/multi-digit-dataset/output_3/test/combined_1_to_8_random_real/"
+train_df_path =         "/home/kaushikdas/aashish/multi-digit-dataset/output_4/train/labels_1_to_8.csv"
+train_image_base_path = "/home/kaushikdas/aashish/multi-digit-dataset/output_4/train/combined_1_to_8_random_real/"
+test_df_path =          "/home/kaushikdas/aashish/multi-digit-dataset/output_4/test/labels_1_to_8.csv"
+test_image_base_path =  "/home/kaushikdas/aashish/multi-digit-dataset/output_4/test/combined_1_to_8_random_real/"
 # Local test paths
 # train_df_path = "/Users/aashishkumar/Documents/Projects/forked_repos/multi-mnist/output_2/test/labels_1.csv"
 # train_image_base_path = "/Users/aashishkumar/Documents/Projects/forked_repos/multi-mnist/output_2/test/1_reshape/"
@@ -25,8 +25,8 @@ model_tflite_name =  "worksheet.multi_digit_model_1_to_8_real.tflite"
 
 # dataloader params
 img_height = 28
-img_width = 140
-num_time_steps = 35  # img_width//4
+img_width = 168
+num_time_steps = 42  # img_width//4
 max_digit_length = 8
 shuffle = True
 
