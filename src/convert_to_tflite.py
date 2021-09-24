63c7851357de086589af736ffcf702830a9b82d3
Aashish Kumar <aashish.kumar1@byjus.com>
2021-09-24 17:14:50 +0530

    update convert to tflite code

--- src/convert_to_tflite.py
+++ src/convert_to_tflite.py
@@ -1,19 +1,38 @@
+import argparse
 import os
-import cv2
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.utils import to_categorical
+from pathlib import Path
 
 
-model_path = "/home/kaushikdas/aashish/multi-mnist/saved_models/transformer_attempt_1/multi_digits_zone_classifier"
-model_tflite_name = "/home/kaushikdas/aashish/multi-mnist/saved_models/transformer_attempt_1/worksheet.multi_digit_zone_transformer_attempt_tf_2.4.tflite"
 
-converter = tf.lite.TFLiteConverter.from_saved_model(model_path)
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
 
-tflite_model = converter.convert()
-# Save the TF Lite model.
-with tf.io.gfile.GFile(model_tflite_name, 'wb') as f:
-    f.write(tflite_model)
+def parse_args():
+    parser = argparse.ArgumentParser()
+ 
+    parser.add_argument("-model_path", help = "Prefix folder name for output", type=str, default=None)
+    parser.add_argument("-tflite_name", help = "Prefix folder name for output", type=str, default=None)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    model_path = args.model_path
+    tflite_name = args.tflite_name
+    path = Path(model_path)
+    parent_folder = str(path.parent.absolute())
+    model_tflite_name = os.path.join(parent_folder, tflite_name)
+
+    print("Starting TFLite Conversion")
+    converter = tf.lite.TFLiteConverter.from_saved_model(model_path)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    tflite_model = converter.convert()
+    # Save the TF Lite model.
+    with tf.io.gfile.GFile(model_tflite_name, 'wb') as f:
+        f.write(tflite_model)
+    
+    print("TFLite Conversion Done")
+    print("Tflite model saved at: ", model_tflite_name)
+
+if __name__ == '__main__':
+    main()
