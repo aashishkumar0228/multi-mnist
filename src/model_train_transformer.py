b16e09427d5a475033227ac6049d01ffe51f0d9b
Aashish Kumar <aashish.kumar1@byjus.com>
2021-09-27 10:56:59 +0530

    add transformer model

---
+++ src/model_train_transformer.py
@@ -0,0 +1,214 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import albumentations as A
+import argparse
+import time
+import tensorflow_addons as tfa
+
+
+
+from multi_digit_dataloader import MultiDigitDataLoader
+from models import build_transformer_ocr_model
+from utils import *
+from config import *
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+ 
+    parser.add_argument("-prefix", help = "Prefix folder name for output", type=str, default=None)
+    args = parser.parse_args()
+    return args
+
+def get_preds(dataset, model):
+    pred_texts = []
+    for batch in dataset:
+        batch_images = batch["image"]
+        preds = model.predict(batch_images)
+        pred_texts_temp = decode_batch_predictions(preds)
+        pred_texts += pred_texts_temp
+    return pred_texts
+
+def main():
+    args = parse_args()
+    prefix = args.prefix
+    if prefix is None:
+        ts = int(time.time())
+        prefix = str(ts)
+
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+    if not os.path.isdir(figure_folder):
+        os.mkdir(figure_folder)
+    
+    output_dir_prefix = output_dir + prefix
+    figure_folder_prefix = figure_folder + prefix
+
+    if not os.path.isdir(output_dir_prefix):
+        os.mkdir(output_dir_prefix)
+    if not os.path.isdir(figure_folder_prefix):
+        os.mkdir(figure_folder_prefix)
+
+    train_transform = A.Compose([
+                                A.RandomBrightnessContrast(brightness_limit=0.2,contrast_limit=0.1, p=1),
+                                A.ShiftScaleRotate(shift_limit_x=0.01, shift_limit_y=0.1, rotate_limit=5),
+                                A.ToFloat(max_value=255)
+                            ])
+
+    test_transform = A.Compose([
+                                A.ToFloat(max_value=255)
+                            ])
+
+    # get train and test dataset
+    train_multi_digit_dataset = MultiDigitDataLoader(df_path = train_df_path,
+                                                    image_base_path = train_image_base_path,
+                                                    batch_size = batch_size,
+                                                    img_height = img_height,
+                                                    img_width = img_width,
+                                                    num_time_steps = num_time_steps,
+                                                    transform = train_transform,
+                                                    max_digit_length = max_digit_length,
+                                                    shuffle = shuffle)
+
+    test_multi_digit_dataset = MultiDigitDataLoader(df_path = test_df_path,
+                                                    image_base_path = test_image_base_path,
+                                                    batch_size = batch_size,
+                                                    img_height = img_height,
+                                                    img_width = img_width,
+                                                    num_time_steps = num_time_steps,
+                                                    transform = test_transform,
+                                                    max_digit_length = max_digit_length,
+                                                    shuffle = shuffle)
+
+    print("Sanity Check Training Data")
+    check_dataset(train_multi_digit_dataset)
+    print("\n\nSanity Check Testing Data")
+    check_dataset(test_multi_digit_dataset)
+    print("\n\n")
+
+    # Get the model
+    model = build_transformer_ocr_model(img_height, num_classes)
+    opt = keras.optimizers.Adam()
+    # moving_avg_opt = tfa.optimizers.MovingAverage(opt)
+    stochastic_avg_opt = tfa.optimizers.SWA(opt)
+    # stochastic_avg_opt = tfa.optimizers.SWA(opt, start_averaging=4, average_period=5)
+
+    # model.compile(optimizer=moving_avg_opt)
+    model.compile(optimizer=stochastic_avg_opt)
+
+    print(model.summary())
+    print("\n\n")
+
+    # Add early stopping
+    # early_stopping = keras.callbacks.EarlyStopping(
+    #     monitor="val_loss", patience=early_stopping_patience, restore_best_weights=True
+    # )
+    # reduce learning rate
+    lrr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss',patience=2,verbose=1,factor=0.5, min_lr=0.00001)
+
+    # Train the model
+    history_1 = model.fit(
+                        train_multi_digit_dataset, 
+                        validation_data=test_multi_digit_dataset,
+                        epochs=epochs, 
+                        callbacks=[lrr]
+                        )
+
+    get_loss_plot(history_1, figure_folder_prefix)
+    create_loss_csv(history_1, figure_folder_prefix)
+
+    # get average weights
+    print("\n\nAveraging layer weights\n\n")
+    # moving_avg_opt.assign_average_vars(model.variables)
+    stochastic_avg_opt.assign_average_vars(model.variables)
+
+    print("\n\nSaving Original Model\n\n")
+    model.save(output_dir_prefix + "/" + model_folder_name + "_original")
+
+    prediction_model = keras.models.Model(
+        model.get_layer(name="image").input, model.get_layer(name="dense2").output
+    )
+    print("\n\n")
+    print(prediction_model.summary())
+    print("\n\n")
+    
+    train_pred_texts = get_preds(train_multi_digit_dataset, prediction_model)
+    df_train_labels = train_multi_digit_dataset.df.copy()
+    df_train_labels['preds'] = train_pred_texts
+
+    test_pred_texts = get_preds(test_multi_digit_dataset, prediction_model)
+    df_test_labels = test_multi_digit_dataset.df.copy()
+    df_test_labels['preds'] = test_pred_texts
+
+    print("\nCalculating Edit Distance\n")
+    train_edit_distance_freq, train_wrong_count = get_edit_distance_freq(df_train_labels)
+    show_edit_distance_freq_graph(train_edit_distance_freq, "train_edit_distance_frequency", figure_folder_prefix)
+
+    test_edit_distance_freq, test_wrong_count = get_edit_distance_freq(df_test_labels)
+    show_edit_distance_freq_graph(test_edit_distance_freq, "test_edit_distance_frequency", figure_folder_prefix)
+
+    print("\nCalculating Word Length Frequency\n")
+    train_correct_word_length_freq, train_wrong_word_length_freq = get_word_leng_freq(df_train_labels)
+    show_word_length_freq_graph(train_wrong_word_length_freq,"train_wrong_word_length_frequency", figure_folder_prefix)
+    show_word_length_freq_graph(train_correct_word_length_freq,"train_correct_word_length_frequency", figure_folder_prefix)
+
+    test_correct_word_length_freq, test_wrong_word_length_freq = get_word_leng_freq(df_test_labels)
+    show_word_length_freq_graph(test_wrong_word_length_freq,"test_wrong_word_length_frequency", figure_folder_prefix)
+    show_word_length_freq_graph(test_correct_word_length_freq,"test_correct_word_length_frequency", figure_folder_prefix)
+
+    total_train_samples = df_train_labels.shape[0]
+    print("Train wrong:", train_wrong_count)
+    print("Train size:", total_train_samples)
+    print("Train Error %:",train_wrong_count/total_train_samples*100)
+    print("Train Accuracy %:",(1 - (train_wrong_count/total_train_samples))*100)
+
+    total_test_samples = df_test_labels.shape[0]
+    print("Test wrong:", test_wrong_count)
+    print("Test size:", total_test_samples)
+    print("Test Error %:",test_wrong_count/total_test_samples*100)
+    print("Test Accuracy %:",(1 - (test_wrong_count/total_test_samples))*100)
+    
+    train_accuracy_file = open(figure_folder_prefix + '/train_test_accuracy.txt', 'w')
+    train_accuracy_file.write("Train wrong: {}\n".format(train_wrong_count))
+    train_accuracy_file.write("Train size: {}\n".format(total_train_samples))
+    train_accuracy_file.write("Train Error %: {}\n".format(train_wrong_count/total_train_samples*100))
+    train_accuracy_file.write("Train Accuracy %: {:.2f}\n\n".format((1 - (train_wrong_count/total_train_samples))*100))
+    train_accuracy_file.write("Test wrong: {}\n".format(test_wrong_count))
+    train_accuracy_file.write("Test size: {}\n".format(total_test_samples))
+    train_accuracy_file.write("Test Error %: {}\n".format(test_wrong_count/total_test_samples*100))
+    train_accuracy_file.write("Test Accuracy %: {:.2f}\n".format((1 - (test_wrong_count/total_test_samples))*100))
+    train_accuracy_file.close()
+
+    # save prediction model
+    print("\n\nSaving Prediction Model\n\n")
+    prediction_model.save(output_dir_prefix + "/" + model_folder_name)
+
+    # model_json = prediction_model.to_json()
+    # with open(output_dir_prefix + "/" + model_json_file_name, "w") as json_file:
+    #     json_file.write(model_json)
+
+    prediction_model.save_weights(output_dir_prefix + "/" + model_weights_file_name)
+
+    # convert to tflite model
+    print("\n\nConverting to TF Lite \n\n")
+    converter = tf.lite.TFLiteConverter.from_keras_model(prediction_model)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+    tflite_model = converter.convert()
+
+    with tf.io.gfile.GFile(output_dir_prefix + "/" + model_tflite_name, 'wb') as f:
+        f.write(tflite_model)
+    
+    print("Done")
+
+
+    
+
+
+if __name__ == '__main__':
+    main()
--- src/models.py
+++ src/models.py
@@ -172,3 +172,187 @@ def build_big_model_no_compile(img_height, num_classes=11):
     # # Compile the model and return
     # model.compile(optimizer=moving_avg_opt)
     return model
+
+
+## Transformer Related Functions
+# Using custom functions for Multi-Head attention layers as the layers.MultiheadAttention API
+# requires special ops to convert to tflite.
+
+def get_angles(pos, i, d_model):
+    angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+    return pos * angle_rates
+
+def positional_encoding(position, d_model):
+    angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                          np.arange(d_model)[np.newaxis, :],
+                          d_model)
+
+    # apply sin to even indices in the array; 2i
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+
+    # apply cos to odd indices in the array; 2i+1
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+
+    pos_encoding = angle_rads[np.newaxis, ...]
+
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+
+def scaled_dot_product_attention(q, k, v, mask=None):
+    """Calculate the attention weights.
+    q, k, v must have matching leading dimensions.
+    k, v must have matching penultimate dimension, i.e.: seq_len_k = seq_len_v.
+    The mask has different shapes depending on its type(padding or look ahead) 
+    but it must be broadcastable for addition.
+
+    Args:
+    q: query shape == (..., seq_len_q, depth)
+    k: key shape == (..., seq_len_k, depth)
+    v: value shape == (..., seq_len_v, depth_v)
+    mask: Float tensor with shape broadcastable 
+          to (..., seq_len_q, seq_len_k). Defaults to None.
+
+    Returns:
+    output, attention_weights
+    """
+
+    matmul_qk = tf.matmul(q, k, transpose_b=True)  # (..., seq_len_q, seq_len_k)
+
+    # scale matmul_qk
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    # add the mask to the scaled tensor.
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)  
+
+    # softmax is normalized on the last axis (seq_len_k) so that the scores
+    # add up to 1.
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)  # (..., seq_len_q, seq_len_k)
+
+    output = tf.matmul(attention_weights, v)  # (..., seq_len_q, depth_v)
+
+    return output, attention_weights
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, num_heads, d_model):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+        assert d_model % self.num_heads == 0
+
+        self.depth = d_model // self.num_heads
+
+        self.wq = tf.keras.layers.Dense(d_model)
+        self.wk = tf.keras.layers.Dense(d_model)
+        self.wv = tf.keras.layers.Dense(d_model)
+
+        self.dense = tf.keras.layers.Dense(d_model)
+        
+    def split_heads(self, x, batch_size):
+        """Split the last dimension into (num_heads, depth).
+        Transpose the result such that the shape is (batch_size, num_heads, seq_len, depth)
+        """
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+    
+    def call(self, v, k, q, mask=None):
+        batch_size = tf.shape(q)[0]
+
+        q = self.wq(q)  # (batch_size, seq_len, d_model)
+        k = self.wk(k)  # (batch_size, seq_len, d_model)
+        v = self.wv(v)  # (batch_size, seq_len, d_model)
+
+        q = self.split_heads(q, batch_size)  # (batch_size, num_heads, seq_len_q, depth)
+        k = self.split_heads(k, batch_size)  # (batch_size, num_heads, seq_len_k, depth)
+        v = self.split_heads(v, batch_size)  # (batch_size, num_heads, seq_len_v, depth)
+
+        # scaled_attention.shape == (batch_size, num_heads, seq_len_q, depth)
+        # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
+        scaled_attention, attention_weights = scaled_dot_product_attention(
+            q, k, v, mask)
+
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])  # (batch_size, seq_len_q, num_heads, depth)
+
+        concat_attention = tf.reshape(scaled_attention, 
+                                      (batch_size, -1, self.d_model))  # (batch_size, seq_len_q, d_model)
+
+        output = self.dense(concat_attention)  # (batch_size, seq_len_q, d_model)
+
+        return output
+
+class TransformerBlock(layers.Layer):
+    def __init__(self, num_heads, key_dim, ff_dim, maximum_position_encoding, rate=0.1, name=None):
+        super().__init__(name=name)
+        self.att = MultiHeadAttention(num_heads=num_heads, d_model=key_dim)
+        self.ffn = keras.Sequential(
+            [layers.Dense(ff_dim, activation="relu"), layers.Dense(ff_dim),]
+        )
+        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = layers.Dropout(rate)
+        self.dropout2 = layers.Dropout(rate)
+        self.pos_encoding = positional_encoding(maximum_position_encoding, ff_dim)
+
+    def call(self, inputs, training):
+        seq_len = tf.shape(inputs)[1]
+        inputs += self.pos_encoding[:, :seq_len, :]
+        attn_output = self.att(inputs, inputs, inputs)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(inputs + attn_output)
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        return self.layernorm2(out1 + ffn_output)
+
+
+def build_transformer_ocr_model(img_height, num_classes=11):
+    # Inputs to the model
+    input_img = layers.Input(shape=(None, img_height, 1), name="image", dtype="float32")
+    labels = layers.Input(name="label", shape=(None,), dtype="float32")
+
+    # First conv block
+    x = layers.Conv2D(32,(3, 3),padding="same",name="Conv1")(input_img)
+    x = layers.ELU(name="elu1")(x)
+    x = layers.Conv2D(32,(3, 3),padding="same",name="Conv2")(x)
+    x = layers.ELU(name="elu2")(x)
+    x = layers.MaxPooling2D((2, 2), name="pool1")(x)
+    x = layers.Dropout(0.2, name="dropout1")(x)
+
+    # Second conv block
+    x = layers.Conv2D(64,(3, 3),padding="same",name="Conv3")(x)
+    x = layers.ELU(name="elu3")(x)
+    x = layers.Conv2D(64,(3, 3),padding="same",name="Conv4")(x)
+    x = layers.ELU(name="elu4")(x)
+    x = layers.MaxPooling2D((2, 2), name="pool2")(x)
+    x = layers.Dropout(0.2, name="dropout2")(x)
+
+    # We have used two max pool with pool size and strides 2.
+    # Hence, downsampled feature maps are 4x smaller. The number of
+    # filters in the last layer is 64. Reshape accordingly before
+    # passing the output to the RNN part of the model
+    # new_shape = ((img_width // 4), (img_height // 4) * 64)
+    new_shape = (-1, x.shape[2]*x.shape[3])
+    x = layers.Reshape(target_shape=new_shape, name="reshape")(x)
+    x = layers.Dense(128, activation="relu", name="dense1")(x)
+    x = layers.Dropout(0.2, name="dropout3")(x)
+
+    # Transformers
+    transformer_block_1 = TransformerBlock(num_heads=2, key_dim=128, ff_dim=128, maximum_position_encoding=80, name='tansformer_block_1')
+    x = transformer_block_1(x)
+    transformer_block_2 = TransformerBlock(num_heads=2, key_dim=128, ff_dim=128, maximum_position_encoding=80, name='tansformer_block_2')
+    x = transformer_block_2(x)
+
+    # Output layer
+    x = layers.Dense(num_classes, activation="softmax", name="dense2")(x)
+
+    # Add CTC layer for calculating CTC loss at each step
+    input_length = layers.Input(name='input_length', shape=[1], dtype='int64')
+    label_length = layers.Input(name='label_length', shape=[1], dtype='int64')
+    output = CTCLayer(name="ctc_loss")(labels, x, input_length, label_length)
+
+    # Define the model
+    model = keras.models.Model(
+        inputs=[input_img, labels, input_length, label_length], outputs=output, name="ocr_model_v1"
+    )
+    return model
