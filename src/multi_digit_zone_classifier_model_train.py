51bf05a6033cc9ad55352ee36250c6060e5bbbaf
Aashish Kumar <aashish.kumar1@byjus.com>
2021-10-06 11:19:04 +0530

    changes

--- src/multi_digit_zone_classifier_model_train.py
+++ src/multi_digit_zone_classifier_model_train.py
@@ -13,6 +13,11 @@ import tensorflow_addons as tfa
 from tqdm import tqdm
 import click
 
+# set random seed
+seed = 3215
+np.random.seed(seed)
+random.seed(seed)
+tf.random.set_seed(seed)
 
 class MultiDigitZoneDataLoader(tf.keras.utils.Sequence):
     def __init__(self, 
@@ -21,11 +26,10 @@ class MultiDigitZoneDataLoader(tf.keras.utils.Sequence):
                  transform,
                  shuffle=True):
         '''
-        df_path: path for dataframe which has file_name and labels
-        image_base_path: folder where images are
-        img_height: height of image
-        img_width: width of image
-        num_time_steps = number of input time steps for lstm layer
+        folder_path: folder where images are
+        num_classes: number of classes
+        transform: transformations to apply on image
+        shuffle: if true shuffle dataset after every epoch
         '''
         self.folder_path = folder_path
         self.num_classes = num_classes
@@ -88,7 +92,7 @@ def build_small_lstm_model(img_height, num_classes):
     x = layers.Conv2D(16,(3, 3),padding="same",name="Conv2")(x)
     x = layers.ELU(name="elu2")(x)
     x = layers.MaxPooling2D((2, 2), name="pool1")(x)
-    x = layers.Dropout(0.2, name="dropout1")(x)
+    x = layers.Dropout(0.2, seed=142, name="dropout1")(x)
 
     # Second conv block
     x = layers.Conv2D(32,(3, 3),padding="same",name="Conv3")(x)
@@ -96,12 +100,12 @@ def build_small_lstm_model(img_height, num_classes):
     x = layers.Conv2D(32,(3, 3),padding="same",name="Conv4")(x)
     x = layers.ELU(name="elu4")(x)
     x = layers.MaxPooling2D((2, 2), name="pool2")(x)
-    x = layers.Dropout(0.2, name="dropout2")(x)
+    x = layers.Dropout(0.2, seed=148, name="dropout2")(x)
     
     new_shape = (-1, x.shape[2]*x.shape[3])
     x = layers.Reshape(target_shape=new_shape, name="reshape")(x)
     x = layers.Dense(64, activation="relu", name="dense1")(x)
-    x = layers.Dropout(0.2, name="dropout3")(x)
+    x = layers.Dropout(0.2, seed=196, name="dropout3")(x)
     
     x = layers.Bidirectional(layers.LSTM(32), name="bidirectional1")(x)
     
@@ -122,6 +126,7 @@ def get_accuracy(dataset, model):
             correct += 1
         else:
             wrong += 1
+    print("Correct", correct, "Wrong", wrong)
     print("accuracy: ", correct/(correct + wrong))
     return correct, wrong
 
@@ -131,7 +136,7 @@ def get_loss_accuracy_plot(history_1, figure_folder=None):
     f.add_subplot(121)
 
     plt.plot(history_1.epoch,history_1.history['accuracy'],label = "accuracy") # Accuracy curve for training set
-    # plt.plot(history_1.epoch,history_1.history['val_accuracy'],label = "val_accuracy") # Accuracy curve for validation set
+    plt.plot(history_1.epoch,history_1.history['val_accuracy'],label = "val_accuracy") # Accuracy curve for validation set
 
     plt.title("Accuracy Curve",fontsize=18)
     plt.xlabel("Epochs",fontsize=15)
@@ -143,7 +148,7 @@ def get_loss_accuracy_plot(history_1, figure_folder=None):
     f.add_subplot(122)
 
     plt.plot(history_1.epoch,history_1.history['loss'],label="loss") # Loss curve for training set
-    # plt.plot(history_1.epoch,history_1.history['val_loss'],label="val_loss") # Loss curve for validation set
+    plt.plot(history_1.epoch,history_1.history['val_loss'],label="val_loss") # Loss curve for validation set
 
     plt.title("Loss Curve",fontsize=18)
     plt.xlabel("Epochs",fontsize=15)
@@ -156,10 +161,12 @@ def get_loss_accuracy_plot(history_1, figure_folder=None):
 @click.command()
 @click.option('--output_dir', default=None, help="Output directory to store artifacts")
 def main(output_dir):
-    train_folder_path = "/Users/aashishkumar/Documents/Projects/forked_repos/multi-mnist/zone_classifier_data/train_5"
+    train_folder_path = "/Users/aashishkumar/Documents/Projects/forked_repos/multi-mnist/zone_classifier_data/set_6/train"
+    test_folder_path = "/Users/aashishkumar/Documents/Projects/forked_repos/multi-mnist/zone_classifier_data/set_6/test"
     num_classes = 3
     img_height = 28
     shuffle = True
+    epochs = 15
     
     # batch size is 1 as each image width is different
 
@@ -174,8 +181,18 @@ def main(output_dir):
                                                          transform = train_transform,
                                                          shuffle = shuffle)
 
+    test_transform = A.Compose([
+                                A.ToFloat(max_value=255)
+                                ])
+    
+    test_multi_digit_dataset = MultiDigitZoneDataLoader(folder_path = test_folder_path,
+                                                         num_classes = num_classes,
+                                                         transform = test_transform,
+                                                         shuffle = shuffle)
+
 
     print("Length train_multi_digit_dataset: ",len(train_multi_digit_dataset))
+    print("Length test_multi_digit_dataset: ",len(test_multi_digit_dataset))
 
     for batch in train_multi_digit_dataset:
         x, y = batch
@@ -190,39 +207,65 @@ def main(output_dir):
     moving_avg_opt = tfa.optimizers.MovingAverage(opt)
     model.compile(optimizer=moving_avg_opt, loss=loss ,metrics=['accuracy'])
     print(model.summary())
-
-    epochs = 10
+    print("-"*20)
+    print("Training Starts")
     lrr = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss',patience=2,verbose=1,factor=0.5, min_lr=0.000001)
-    history_1 = model.fit(train_multi_digit_dataset, epochs=epochs, callbacks=[lrr])
-
+    history_1 = model.fit(
+                          train_multi_digit_dataset, 
+                          validation_data=test_multi_digit_dataset, 
+                          epochs=epochs, 
+                          callbacks=[lrr]
+                          )
+    print("Training End")
+    print("-"*20)
     get_loss_accuracy_plot(history_1, output_dir)
 
+    print("Calculate Accuracy")
     get_accuracy(train_multi_digit_dataset, model)
+    print("Moving Average Optimizer Weight Update")
     moving_avg_opt.assign_average_vars(model.variables)
-    get_accuracy(train_multi_digit_dataset, model)
+    print("Calculate Accuracy")
+    train_correct_count, train_wrong_count = get_accuracy(train_multi_digit_dataset, model)
+    test_correct_count, test_wrong_count = get_accuracy(test_multi_digit_dataset, model)
+    
+    total_train_samples = train_correct_count + train_wrong_count
+    total_test_samples = test_correct_count + test_wrong_count
+    print("Train wrong:", train_wrong_count)
+    print("Train size:", total_train_samples)
+    print("Train Error %:",train_wrong_count/total_train_samples*100)
+    print("Train Accuracy %:",(1 - (train_wrong_count/total_train_samples))*100)
+    print("Test wrong:", test_wrong_count)
+    print("Test size:", total_test_samples)
+    print("Test Error %:",test_wrong_count/total_test_samples*100)
+    print("Test Accuracy %:",(1 - (test_wrong_count/total_test_samples))*100)
 
-    # prefix = "lstm_attempt_6"
-    # base_folder = "/Users/aashishkumar/Documents/Projects/forked_repos/multi-mnist/zone_classifier_model/"
-    # base_folder_name = base_folder + prefix + "/"
-    # if not os.path.isdir(base_folder_name):
-    #     os.mkdir(base_folder_name)
-    # model_tflite_name = base_folder_name + "worksheet.multi_digit_zone_clf_" + prefix + ".tflite"
-    # model_folder_name = base_folder_name + "multi_digits_zone_classifier"
-    # model_json_file_name = base_folder_name + "multi_digits_zone_classifier_json.json"
-    # model_weights_file_name = base_folder_name + "multi_digits_zone_classifier_weights.h5"
+    train_accuracy_file = open(os.path.join(output_dir, "train_test_accuracy.txt"), 'w')
+    train_accuracy_file.write("Train wrong: {}\n".format(train_wrong_count))
+    train_accuracy_file.write("Train size: {}\n".format(total_train_samples))
+    train_accuracy_file.write("Train Error %: {}\n".format(train_wrong_count/total_train_samples*100))
+    train_accuracy_file.write("Train Accuracy %: {:.2f}\n\n".format((1 - (train_wrong_count/total_train_samples))*100))
+    train_accuracy_file.write("Test wrong: {}\n".format(test_wrong_count))
+    train_accuracy_file.write("Test size: {}\n".format(total_test_samples))
+    train_accuracy_file.write("Test Error %: {}\n".format(test_wrong_count/total_test_samples*100))
+    train_accuracy_file.write("Test Accuracy %: {:.2f}\n".format((1 - (test_wrong_count/total_test_samples))*100))
+    train_accuracy_file.close()
 
-    model_tflite_name = os.path.join(output_dir, "worksheet.multi_digit_zone_clf.tflite")
+    model_tflite_name = os.path.join(output_dir, "worksheet.hw_ocr_multi_digits_zone_clf.tflite")
     model_folder_name = os.path.join(output_dir, "multi_digits_zone_classifier")
     model_json_file_name = os.path.join(output_dir, "multi_digits_zone_classifier_json.json")
     model_weights_file_name = os.path.join(output_dir, "multi_digits_zone_classifier_weights.h5")
 
+    print("Saving Model")
     model.save(model_folder_name)
+    print("Saving Model Json")
     model_json = model.to_json()
     with open(model_json_file_name, "w") as json_file:
         json_file.write(model_json)
     
+    print("Saving Model Weights")
     model.save_weights(model_weights_file_name)
 
+    print("Converting to TFLite")
     converter = tf.lite.TFLiteConverter.from_saved_model(model_folder_name)
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
     tflite_model = converter.convert()
@@ -230,6 +273,8 @@ def main(output_dir):
     # Save the TF Lite model.
     with tf.io.gfile.GFile(model_tflite_name, 'wb') as f:
         f.write(tflite_model)
+    
+    print("Done")
     
 
 if __name__ == '__main__':
